ae67ea57b4087bde274ff57a3f2f24b6fec26f55
Mufuniwa8 <mufuniwambedzi@gmail.com>
2023-05-22 16:40:04 +0200

    js-script

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+let display  = document.getElementById('screen');
+let clearAll = (placeholder) => {
+    display.value = '';
+}
+ let del = () => {
+    display.value=display.value.slice(0, -1);
+ }
+let myFunction = (myFunction) => {
+    display.value += myFunction;
+}
 
-
-// function myFunction (min, max) {
-//     return Math.random () * ;
-// }
-
+let calcAnswer = () => {
+    display.value = eval(display.value);
+}
+  
  
